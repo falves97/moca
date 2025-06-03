0f516f4f19194ab521dafb6b6a2bf895a5a1b1bb
Fernando Braga Alves <fbragaalves97@gmail.com>
2025-06-03 07:50:12 -0300

    feat: troca de banco para percona

--- migrations/Version20250603104812.php
+++ migrations/Version20250603104812.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250603103152 extends AbstractMigration
+final class Version20250603104812 extends AbstractMigration
 {
     public function getDescription(): string
     {
